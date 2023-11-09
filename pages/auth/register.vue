3adb9effc9a950c6c76a369fbef47bae38ccd056
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2023-11-09 15:24:34 +0330

    add server folder

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -2,22 +2,22 @@
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-md-4">
-                <form>
+                <form @submit.prevent="register">
                     <div class="mb-3">
                         <label htmlFor="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" />
+                        <input type="text" class="form-control" id="name" v-model="formdata.name" />
                     </div>
                     <div class="mb-3">
                         <label htmlFor="email" class="form-label">Email address</label>
-                        <input type="text" class="form-control" id="email" />
+                        <input type="text" class="form-control" id="email"  v-model="formdata.email" />
                     </div>
                     <div class="mb-3">
                         <label htmlFor="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" />
+                        <input type="password" class="form-control" id="password"  v-model="formdata.password" />
                     </div>
                     <div class="mb-3">
                         <label htmlFor="c_password" class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control" id="c_password" />
+                        <input type="password" class="form-control" id="c_password"  v-model="formdata.c_password" />
                     </div>
                     <button class="btn btn-primary">
                         Register
@@ -27,3 +27,19 @@
         </div>
     </div>
 </template>
+
+
+
+
+<script setup>
+const formdata = reactive({
+name : "",
+email : "",
+password : "",
+c_password : ""
+
+})
+function register(){
+    console.log(formdata);
+}
+</script>
