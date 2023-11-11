b832b1ab8c39c73c471f63594eba200c726924b9
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2023-11-11 18:41:05 +0330

    create logout script

--- components/Header.vue
+++ components/Header.vue
@@ -2,15 +2,14 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container">
             <NuxtLink class="navbar-brand" to="/">Nuxt-auth</NuxtLink>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <NuxtLink class="nav-link"  active-class="active" to="/">Home</NuxtLink>
+                        <NuxtLink class="nav-link" active-class="active" to="/">Home</NuxtLink>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Posts</a>
@@ -21,7 +20,7 @@
                         <a class="nav-link" href="#">Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" @click="logout" href="#">Logout</a>
                     </li>
                     <li class="nav-item">
                         <NuxtLink active-class="active" class="nav-link" to="/auth/register">Register</NuxtLink>
@@ -34,3 +33,17 @@
         </div>
     </nav>
 </template>
+
+
+<script setup>
+import { useToast } from 'vue-toastification';
+const toast = useToast()
+
+async function logout() {
+    await useFetch('/api/auth/logout', {
+        method: 'POST'
+    });
+    toast.warning("You are logouted!")
+    return navigateTo('/')
+}
+</script>
